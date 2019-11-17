a747fd85610af54d8a2a122f46cad3c1c8c11585
spencer-mcguire <smcguire1826@gmail.com>
2019-11-17 14:14:00 -0600

    basic layout for dashboard and home style

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,37 @@
 import React from "react";
+import styled from "styled-components";
 
 function Home(props) {
+  const Splash = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background: rgb(63, 94, 251);
+    background: linear-gradient(
+      90deg,
+      rgba(63, 94, 251, 1) 32%,
+      rgba(70, 250, 252, 1) 100%
+    );
+    height: 81vh;
+  `;
+
+  const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 25%;
+    align-items: center;
+    justify-content: space-around;
+  `;
+
+  const Button = styled.button`
+    border: none;
+    border-radius: 4px;
+    margin: 2%;
+    padding: 10px 25px;
+    box-shadow: 10px 10px 28px -12px rgba(0, 0, 0, 0.75);
+  `;
+
   const routeToLogin = () => {
     props.history.push("./login");
   };
@@ -9,11 +40,13 @@ function Home(props) {
   };
 
   return (
-    <div>
+    <Splash>
       <h1>Welcome to Lendr!</h1>
-      <button onClick={routeToLogin}>Login</button>
-      <button onClick={routeToSignUp}>Sign Up</button>
-    </div>
+      <ButtonContainer>
+        <Button onClick={routeToLogin}>Login</Button>
+        <Button onClick={routeToSignUp}>Sign Up</Button>
+      </ButtonContainer>
+    </Splash>
   );
 }
 
